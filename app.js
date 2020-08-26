c34d228d3685abb06c373026305c178933736f3b
waqaskhalidabc <67595716+waqaskhalidabc@users.noreply.github.com>
2020-08-26 22:46:16 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+
 var logger = require('morgan');
 var mongoose=require('mongoose')
 var loginRouter = require('./routes/login');
@@ -15,8 +15,7 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 
@@ -39,7 +38,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+ 
   res.status(err.status || 500);
   res.render('error');
 });
